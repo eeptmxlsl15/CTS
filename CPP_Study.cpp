b9f8d01b9e2bf3551fd22ca4e537064d5511819b
김석진 <69078017+eeptmxlsl15@users.noreply.github.com>
2023-03-12 22:39:02 +0900

    네트워크

--- CPP_Study.cpp
+++ CPP_Study.cpp
@@ -1,28 +1,33 @@
-﻿#include <iostream>
-#include <algorithm>
-#include <vector>
-
+﻿#include <vector> 
 using namespace std;
 
-int map[100][100];
+bool check[200];
 
-int solution(int m, int n, vector<vector<int>> puddles) {
-    map[2][1] = 1; //시작점 위 아래 1로 시작
-    map[1][2] = 1;
+void dfs(int curComputer, int n, vector< vector<int> > computers) {
+    // 현재 컴퓨터를 가르키는 인덱스에 check
+    check[curComputer] = true;
 
-
-    for (int i = 0; i < puddles.size(); i++)
-        map[puddles[i][0]][puddles[i][1]] = -1;//웅덩이 위치에 -1
-
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (map[i][j] == -1) continue;//웅덩이 지나감
-            if (map[i][j - 1] != -1)
-                map[i][j] = (map[i][j] + map[i][j - 1]) % 1000000007;
-            if (map[i - 1][j] != -1)
-                map[i][j] = (map[i][j] + map[i - 1][j]) % 1000000007;
+    for (int i = 0; i < n; i++) {
+        
+        if (check[i] == false && computers[curComputer][i] == 1) {
+            //탐색하지 않았고 현재 컴퓨터와 연결되어있는 컴퓨터 찾기
+            dfs(i, n, computers);
         }
-
     }
-    return map[m][n];
+}
+
+int solution(int n, vector< vector<int> > computers) {
+    int answer = 0;
+
+    for (int i = 0; i < n; i++) {
+        
+        if (check[i] == false) {//검사하지 않았을때
+            dfs(i, n, computers);
+
+            //dfs가 연결된 점들을 다 돌고 끝났을때 +1
+            answer++;
+        }
+    }
+
+    return answer;
 }

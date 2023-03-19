aa52c5fa1c24c9ca0256cd3f2add9d5cf78b9341
김석진 <69078017+eeptmxlsl15@users.noreply.github.com>
2023-03-20 01:12:20 +0900

    Update Hide.cpp

--- Hide.cpp
+++ Hide.cpp
@@ -8,15 +8,15 @@ using namespace std;
 queue<int> que;
 stack<int> path;
 int time = 0;
-bool visit[100000];
-int parent[100000];
+bool visit[100001];
+int parent[100001];
 
-int N, K;
+int N=0, K=0;
 
 
 void bfs() {
     while (!que.empty()) {
-        int first = que.front();//°¡Àå ¸ÕÀú µé¾î¿Â °ª º¹»ç
+        int first = que.front();//가장 먼저 들어온 값 복사
         que.pop();
         
         if (first == K) {
@@ -30,17 +30,17 @@ void bfs() {
         }
 
 
-        if (first*2 >= 0 && visit[first*2] == false && first*2 <= 100000) {
+        if (first*2 >= 0 && visit[first*2] == false && first*2 < 200002) {
             que.push(first*2);
             parent[first * 2] = first;
             visit[first*2] = true;
         }
-        if (first + 1 >= 0 && visit[first + 1] == false && first + 1 <= 100000) {
+        if (first + 1 >= 0 && visit[first + 1] == false && first + 1 < 100001) {
             que.push(first + 1);
             parent[first +1] = first;
             visit[first +1] = true;
         }
-        if (first - 1 >= 0 && visit[first - 1] == false && first - 1 <= 100000) {
+        if (first - 1 >= 0 && visit[first - 1] == false && first - 1 < 100001) {
             que.push(first - 1);
             parent[first - 1] = first;
             visit[first -1] = true;
@@ -51,9 +51,11 @@ void bfs() {
     }
 }
 
-
 int main() {
-    cin >> N >> K;
+    
+        cin >> N >> K;
+    
+        
     que.push(N);
     bfs();
     cout << time << endl;
@@ -62,4 +64,5 @@ int main() {
         path.pop();
         
     }
+    return 0;
 }
